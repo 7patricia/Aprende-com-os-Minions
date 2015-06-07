d0e11167e120eb9c3669d193b3515f9bdaa4f795
Sandra95 <sandraisabelferreira95@gmail.com>
2015-06-07 06:53:31 +0100

    Commit de cenas
    Esqueci me de lago que me pediste, mas mal venha faco isso

--- 3ªFase/povoarBD.sql
+++ 3ªFase/povoarBD.sql
@@ -1,8 +1,8 @@
+
+TABELA  DAS AULAS
 /*Aulas-Primetro*/
 insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Perimetro',1,'Perimetro','https://photos-5.dropbox.com/t/2/AADH-m109c6v7aYWESb-fOuoM51-DtoOByr2_LgDpzCHmg/12/216592353/png/32x32/1/_/1/2/aula%20-%20perimetro%20-%20grau1.PNG/COHfo2cgASACIAMgBCAFIAYgBygBKAI/scdH2JcW58jpzNY6Ij9Hr_47IGHh_Art6Hr29cu6esw?size=1024x768&size_mode=2','https://youtu.be/_CJryeE0ZX0');
-
 insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Perimetro',2,'Perimetro','https://photos-6.dropbox.com/t/2/AAB47f3neWqXJ1NU_c5Kv3AOkSDaeazO2EUF8ldmZecgiA/12/216592353/png/32x32/1/_/1/2/aula%20-%20perimetro%20-%20grau2.PNG/COHfo2cgASACIAMgBCAFIAYgBygBKAI/08aBFAP5RHbeMYPlgvS-98RT2lvE5chWkKwIl13EFGs?size=1024x768&size_mode=2','https://youtu.be/Q3eBfZKBqYs');
-
 insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Perimetro',3,'Perimetro','https://photos-4.dropbox.com/t/2/AAC1F2aZ0LLl34-Ck-oUH8M0ejRoRwxyTNw56si070DZWQ/12/216592353/png/32x32/1/_/1/2/aula%20-%20perimetro%20-%20grau3.PNG/COHfo2cgASACIAMgBCAFIAYgBygBKAI/wxzVuyd4PxRPqrv88ioQAtez9LRJ8fNjeP4R3yxQT-E?size=1024x768&size_mode=2','https://youtu.be/5IVm-Y5EqF4');
 
 /*Aulas-Area*/
@@ -15,7 +15,7 @@ insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Area',3,'Area
 insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Medidas de Comprimento',1,'Medidas de Comprimento','https://photos-2.dropbox.com/t/2/AAAZeg0zPj5uNY85Cvw5HOJr3Ush1mQOo8To0e03oUjuxA/12/216592353/png/32x32/1/_/1/2/aula%20-%20transformacoes%20-%20grau1.PNG/COHfo2cgASACIAMgBCAFIAYgBygBKAI/mWMEwcUpPeLkWBNW7iNjLXJLEn9NITMy4aw3E3CaczY?size=1024x768&size_mode=2','https://youtu.be/0HxYMq_dx0Q');
 insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Medidas de Comprimento',2,'Medidas de Comprimento','https://photos-2.dropbox.com/t/2/AACHj298uG-hdEa-2S7KSbSwEc2RDcsTDVUZSjlP_7aqrw/12/216592353/png/32x32/1/_/1/2/aula%20-%20transformacoes%20-%20grau2.PNG/COHfo2cgASACIAMgBCAFIAYgBygBKAI/WFiAbUVfXZHEu7m_DrPKtNSvIpSIDWidTAZ5GY_DafY?size=800x600&size_mode=2','https://youtu.be/5wBpuwrxoGk');
 
-
+TABELA DAS DUVIDAS
 /* Exercicios- Perimetro*/
 insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Exercicios sobre Perimetros',1,'Exercicios sobre Perimetros','https://photos-6.dropbox.com/t/2/AABtHQ4tbHDG7vVFg_RghP_jy6Rh1NSksMLL-zs4MF2Bsw/12/216592353/png/32x32/1/_/1/2/Exercicio-%20Perimetro%20-%20Grau%201.PNG/COHfo2cgASACIAMgBCAFIAYgBygBKAI/avXgv_0L7J0THziv5pa7wyQeMO5i14NxRzykKyNx7Vw?size=800x600&size_mode=2','https://youtu.be/0uauWawzgcU');
 insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Exercicios sobre Perimetros',2,'Exercicios sobre Perimetros','https://photos-6.dropbox.com/t/2/AAB7zJ8ZcTrwvKxedYovXuIRRBZJOnnYSTi0eq9JdrS6Qw/12/216592353/png/32x32/1/_/1/2/Exercicio-%20Perimetro%20-%20Grau2.PNG/COHfo2cgASACIAMgBCAFIAYgBygBKAI/Q88uGVsgb7jh5XEhD7QymBBivaoznvK231cSyquOpqc?size=800x600&size_mode=2','https://youtu.be/Lp2mBsGN9dI');
@@ -26,4 +26,24 @@ insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Exercicios so
 insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Exercicios sobre Áreas',2,'Exercicios sobre Áreas','https://photos-3.dropbox.com/t/2/AABTaKnDilBOPrAOdp2eY73PfXbRM8nLEw2tXatQtGJFNw/12/216592353/png/32x32/1/_/1/2/Exercicio-%20area%20-%20Grau%202.PNG/COHfo2cgASACIAMgBCAFIAYgBygBKAI/chg5Y_gIStHqHSJOcaC_i5FQMyPevwIuPEkSmB_AZVs?size=800x600&size_mode=2','https://youtu.be/Ade73hv-LzU');
 insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Exercicios sobre Áreas',3,'Exercicios sobre Áreas','https://photos-6.dropbox.com/t/2/AAB5Gsq0dTBpRqJV8PBZ1PevEpjqF-y_8pwbZTxJpXR-qw/12/216592353/png/32x32/1/_/1/2/Exercicio-%20area%20-%20Grau3.PNG/COHfo2cgASACIAMgBCAFIAYgBygBKAI/3vlu3moo3kc1DzVTFeydEyMdLZN4__lcU4_AE1Z0ELE?size=800x600&size_mode=2','https://youtu.be/_CVrt0V7J68');
 
+//grau 1
+insert into Perguntas(Descricao, URLImagem, RespCerta) values ('Considere este triângulo com 3 lados iguais (equilátero), sendo x = 3 cm. Qual o valor do perímetro?. Descobre a opção correta e assinala a verdadeira:','https://www.flickr.com/photos/133086819@N04/18502996666/in/album-72157651792039274/','9 cm');
+insert into Perguntas(Descricao, URLImagem, RespCerta) values ('Descobre o perímetro do retângulo, sendo y = 5 cm e x = 4 cm.','https://www.flickr.com/photos/133086819@N04/18524964432/in/album-72157651792039274/','18 cm');
+insert into Perguntas(Descricao, URLImagem, RespCerta) values ('Calcule o perímetro do círculo, com raio, x = 1 cm.','https://www.flickr.com/photos/133086819@N04/17906920694/in/album-72157651792039274/','2π cm');
 
+
+//grau 2
+insert into Perguntas(Descricao, URLImagem, RespCerta) values ('Descobre o perímetro da figura. Tem atenção, que é um triângulo isósceles, com x = 5 cm e y = 3 cm.','https://www.flickr.com/photos/133086819@N04/17906921414/in/album-72157651792039274/','13 cm');
+insert into Perguntas(Descricao, URLImagem, RespCerta) values ('Descobre o perímetro, sendo x = 5 cm e y = 3 cm.','https://www.flickr.com/photos/133086819@N04/17906694084/in/album-72157651792039274/','16 cm');
+insert into Perguntas(Descricao, URLImagem, RespCerta) values ('Descobre o perímetro, sendo x = 1 dm','https://www.flickr.com/photos/133086819@N04/17906920694/in/album-72157651792039274/','2π dm');
+
+
+
+
+
+
+
+
+
+
+insert into Perguntas(Descricao, URLImagem, RespCerta) values ('','','');
