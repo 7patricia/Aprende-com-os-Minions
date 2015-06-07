3a036ac52abab600e1ffd546e67381944949b4b2
Tiago Cunha <tiago.jr.cunha@gmail.com>
2015-06-08 00:04:58 +0100

    Up - povoarBD

--- 3ªFase/povoarBD.sql
+++ 3ªFase/povoarBD.sql
@@ -1,4 +1,4 @@
-use minions;
+
 /*Aulas-Primetro*/
 insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Perimetros',1,'Perimetro 1','https://flic.kr/p/tWK4pC','https://youtu.be/_CJryeE0ZX0');
 insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Perimetros',2,'Perimetro 2','https://flic.kr/p/thufQX','https://youtu.be/Q3eBfZKBqYs');
@@ -11,7 +11,7 @@ insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Areas',3,'Are
 
 /*Aula-MedidasComprimento*/
 
-insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Medidas',1,'Medidas de Comprimento 1','https://flic.kr/p/thiW37','https://youtu.be/0HxYMq_dx0Q');
+insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Medidas',1,'https://flic.kr/p/thiW37','https://youtu.be/0HxYMq_dx0Q');
 insert into Aulas(Tema,GrauDif,Titulo,URLImagem,URLVideo) values ('Medidas',2,'Medidas de Comprimento 2','https://flic.kr/p/uc1bjo','https://youtu.be/5wBpuwrxoGk');
 
 /*Exercicios */
@@ -23,9 +23,10 @@ insert into Exercicios(GrauDif,Tema,URLImagem) values (2,'Areas','https://flic.k
 insert into Exercicios(GrauDif,Tema,URLImagem) values (3,'Areas','https://flic.kr/p/tWVGHB');
 insert into Exercicios(GrauDif,Tema,URLImagem) values (1,'Medidas','https://flic.kr/p/thwRrM');
 insert into Exercicios(GrauDif,Tema,URLImagem) values (2,'Medidas','https://flic.kr/p/tWMEJb');
+/*insert into Exercicios(GrauDif,Tema,URLImagem) values (2,'Medidas','https://flic.kr/p/tWMEJb');
 
 /* Dúvidas- Perimetro*/
-/*insert into Duvidas(Descricao,URLVideo,Exercicio) values ('Exercicio sobre Perimetros','https://youtu.be/0uauWawzgcU',1);
+insert into Duvidas(Descricao,URLVideo,Exercicio) values ('Exercicio sobre Perimetros','https://youtu.be/0uauWawzgcU',1);
 insert into Duvidas(Descricao,URLVideo,Exercicio) values ('Exercicios sobre Perimetros','https://youtu.be/Lp2mBsGN9dI',2);
 insert into Duvidas(Descricao,URLVideo,Exercicio) values ('Exercicios sobre Perimetros','https://youtu.be/dXbRepTkpTo',3);
 
@@ -33,7 +34,7 @@ insert into Duvidas(Descricao,URLVideo,Exercicio) values ('Exercicios sobre Peri
 insert into Duvidas(Descricao,URLVideo,Exercicio_ExercicioID) values ('Exercicio sobre Áreas','https://youtu.be/1d7IFhly4II',4);
 insert into Duvidas(Descricao,URLVideo,Exercicio_ExercicioID) values ('Exercicio sobre Áreas','https://youtu.be/Ade73hv-LzU',5);
 insert into Duvidas(Descricao,URLVideo,Exercicio_ExercicioID) values ('Exercicio sobre Áreas','https://youtu.be/_CVrt0V7J68',6);
-*/
+
 /*Perguntas*/
 insert into Perguntas(Descricao, URLImagem, RespCerta,Exercicio_ExercicioID) values ('Considere este triângulo com 3 lados iguais (equilátero), sendo x = 3 cm. Qual o valor do perímetro?. Descobre a opção correta e assinala a verdadeira:','https://flic.kr/p/uc3GgU','9 cm',1);
 insert into Perguntas(Descricao, URLImagem, RespCerta,Exercicio_ExercicioID) values ('Descobre o perímetro do retângulo, sendo y = 5 cm e x = 4 cm.','https://flic.kr/p/udZhwu','18 cm',1);
@@ -67,6 +68,11 @@ insert into Perguntas (Descricao, URLImagem, RespCerta,Exercicio_ExercicioID) va
 insert into Perguntas (Descricao, URLImagem, RespCerta,Exercicio_ExercicioID) values ('O prof. Manuel comprou um móvel e quer pô-lo no seu gabinete, mas para isso precisa de saber se tem espaço para o pôr. O que ele sabe é que no seu gabinete tem 10 m2 livres e o móvel tem 100 dm2. Podes ajudar a fazer a transformação da área do móvel para m2, para ajudares o professor com a sua compra? ','https://flic.kr/p/uey2Pr','A',8);
 insert into Perguntas (Descricao, URLImagem, RespCerta,Exercicio_ExercicioID) values ('Esta é a minha casa, podias - me dizer quantos cm2 tem a minha cozinha?','https://flic.kr/p/uey2Pr','87100 cm2',8);
 insert into Perguntas (Descricao, URLImagem, RespCerta,Exercicio_ExercicioID) values ('Queria ver se tinha espaço para pôr 20 cartazes deste filme na parede do meu quarto. Sabendo que os 20 cartazes fazem uma área de 10000 cm2, e que a parede do meu quarto tem 500 dm2. Converte as duas medidas para m2, primeiro dos cartazes depois da parede, para me ajudares no meu problema.','https://flic.kr/p/tWMEJb','1 m2 e 5 m2',8);
+
+insert into Perguntas (Descricao, URLImagem, RespCerta,Exercicio_ExercicioID) values ('O Sr. Serafim está a fazer um estudo para descobrir qual o edifício que mais área ocupa na cidade de Lustosa. Para isso, teve que fazer um cálculo a todos os edifícios dessa cidade e foi filtrando até que ficou com 4 edifícios com uma área muita parecida. Visto que ele tem que apresentar este estudo ao município, e tem pouco tempo para o fazer, ele espera que o ajudes a dar a resposta correta.
+Sempre consegues ajudar o Sr. Serafim?','https://flic.kr/p/tWVMKv','A',9);
+insert into Perguntas (Descricao, URLImagem, RespCerta,Exercicio_ExercicioID) values ('Vimos pedir-te ajuda para o planeamento da cidade, transformando as medidas para m2, é só converter o edifício A e C:','https://flic.kr/p/tWVMKv','100m2 e 50m2',9);
+insert into Perguntas (Descricao, URLImagem, RespCerta,Exercicio_ExercicioID) values ('Segundo ordens do presidente da câmara, será demolido um edifício para a construção de uma escola, é necessário 50 m2, indica o edifico com as medidas mais próximas:','https://flic.kr/p/tWVMKv','C',9);	
 
 /*Respostas*/
 
@@ -102,7 +108,10 @@ insert into Respostas(RespA,RespB,RespC,RespD,Pergunta_PerguntaID)values ('11 m'
 insert into Respostas(RespA,RespB,RespC,RespD,Pergunta_PerguntaID)values ('32.4 dm','324 dam','32.4 dam','3240 cm',22);
 insert into Respostas(RespA,RespB,RespC,RespD,Pergunta_PerguntaID)values ('10 m2','0.1 m2','10 m2','100 m2',23);
 insert into Respostas(RespA,RespB,RespC,RespD,Pergunta_PerguntaID)values ('8.71 cm2','871 cm2','8710 cm2','87100 cm2',24);
-
+/*Medidas 3*/
+insert into Respostas(RespA,RespB,RespC,RespD,Pergunta_PerguntaID)values ('A','B','C','D',25);
+insert into Respostas(RespA,RespB,RespC,RespD,Pergunta_PerguntaID)values ('100 m2 e 500 m2','10 m2 e 50m2','100m2 e 50m2','1 m2 e 5 m2',26)
+insert into Respostas(RespA,RespB,RespC,RespD,Pergunta_PerguntaID)values ('A','B','C','D',27);;
 
 
 
